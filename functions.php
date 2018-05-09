b446fb5c020ef62206530c7b2c85fbe7882547d1
frkodes <frkalderon@gmail.com>
2018-05-09 16:13:58 -0500

    solving some background issues on product categories

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function register_my_menus() {
 add_action( 'init', 'register_my_menus' );
 
 function get_my_styles_and_scripts() {
-	wp_enqueue_style( 'style.css', get_stylesheet_uri(), array(), '1.6');
+	wp_enqueue_style( 'style.css', get_stylesheet_uri(), array(), '1.8');
 	wp_enqueue_script( 'all.js', get_template_directory_uri() . '/all.js', array(), '1.0.2', true );
 }
 add_action( 'wp_enqueue_scripts', 'get_my_styles_and_scripts' );
